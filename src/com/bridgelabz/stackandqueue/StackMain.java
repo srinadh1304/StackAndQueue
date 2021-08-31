bda1b5cd80bb3cd33ffcb6834a1267b73c995573
srinadh1304 <srinadh1304@gmail.com>
2021-08-31 22:13:33 +0530

    ADDED:Welcome Message

---
+++ src/com/bridgelabz/stackandqueue/StackMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.stackandqueue;
+
+public class StackMain {
+	public static void main(String args[]) {
+		System.out.println("****** Welcome to Stack and Queue Program *****");
+	}
+}
