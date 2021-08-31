fad1920e0f1944ce56f89be745bf2568b8db0272
srinadh1304 <srinadh1304@gmail.com>
2021-08-31 22:38:02 +0530

    ADDED:Stack operations methods using linked list

--- src/com/bridgelabz/stackandqueue/Stack.java
+++ src/com/bridgelabz/stackandqueue/Stack.java
@@ -3,7 +3,6 @@ package com.bridgelabz.stackandqueue;
 import com.bridgelabz.linkedlist.*;
 
 public class Stack<K> {
-	
 	private final MyLinkedList stackLinkedList;
 	
 	public Stack() {
@@ -14,7 +13,32 @@ public class Stack<K> {
 	public void push(Node element) {
 		stackLinkedList.add(element);
 	}
-
+	
+	
+	public Node peek() {
+		return stackLinkedList.head;
+	}
+	
+	
+	public Node pop() {
+		return stackLinkedList.pop();
+	}
+	
+	
+	public boolean isEmpty() {
+		if(stackLinkedList.head != null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	
+	public int size() {
+		return stackLinkedList.size();
+	}
+	
 	
 	public void printStack() {
 		stackLinkedList.display();
