f647077a44d233cb4e8c628f981058edd048208a
srinadh1304 <srinadh1304@gmail.com>
2021-08-31 22:42:03 +0530

    ADDED:Using pop method to delete stack

--- src/com/bridgelabz/stackandqueue/StackMain.java
+++ src/com/bridgelabz/stackandqueue/StackMain.java
@@ -12,5 +12,15 @@ public class StackMain {
 		stackList.push(secondNode);
 		stackList.push(thirdNode);
 		stackList.printStack();
+		System.out.println("Top element of stack: "+stackList.peek().getKey());
+		System.out.println("Size of the stack is: "+stackList.size());
+		System.out.println("Stack after repeated popping : ");
+		while(!stackList.isEmpty()) {
+
+			stackList.printStack();
+			stackList.pop();
+
+		}
+		stackList.printStack();
 	}
 }
